978a701e885f7dbe74519fa4e6fa62e1d2b023a3
Cameron Perot <cameronperot@protonmail.ch>
2019-04-20 23:58:40 +0200

    Modified Network type to keep track of largest cluster size

--- code/Percolation.jl/src/graphs.jl
+++ code/Percolation.jl/src/graphs.jl
@@ -13,6 +13,7 @@ mutable struct Network <: Graph
 		`nodes`   : Set of nodes present in the graph
 		`edges`   : Set of edges present in the graph
 		`clusters`: Set of clusters present in the graph
+		`C`       : Largest cluster size
 		`P`       : Order parameter
 		`rng`     : Random number generator
 	"""
@@ -20,14 +21,16 @@ mutable struct Network <: Graph
 	nodes    ::Set{Int}
 	edges    ::Set{Set{Int}}
 	clusters ::Set{Set{Int}}
+	C        ::Array{Int, 1}
 	P        ::Array{Float64, 1}
 	rng      ::MersenneTwister
 	function Network(n::Int; seed=8)
 		nodes    = Set(1:n)
-		edges    = Set{Tuple{Int, Int}}()
+		edges    = Set()
 		clusters = Set([Set(i) for i in 1:n])
-		P        = [1/n]
+		C        = []
+		P        = []
 		rng      = MersenneTwister(seed)
-		new(n, nodes, edges, clusters, P, rng)
+		new(n, nodes, edges, clusters, C, P, rng)
 	end
 end
