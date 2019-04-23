4d23383ed2f660f24feee555debe72fa35adbfb1
Cameron Perot <cameronperot@protonmail.ch>
2019-04-23 17:02:54 +0200

    Modified functions to account for tau parameter in Network type

--- code/Percolation.jl/src/cluster_functions.jl
+++ code/Percolation.jl/src/cluster_functions.jl
@@ -1,18 +1,18 @@
 function get_cluster(g::Network, node::Int)
 	"""
-	Determines the cluster to which the given node is a member of
+	Determines the cluster which `node` is a member of
 	INPUT
 		`g`   : An instance of type Network
 		`node`: Integer index of the desired node
 	OUTPUT
-		`cluster`: The cluster the given node is a member of
+		`cluster`: The cluster `node` is a member of
 	"""
 	for cluster in g.clusters
 		if node ∈ cluster
 			return cluster
 		end
 	end
-	return node
+	return Set(node)
 end
 
 
@@ -25,15 +25,17 @@ function get_largest_clusters(g::Network, n_clusters::Int)
 	OUTPUT
 		Sorted (descending) array of the `n_clusters` largest clusters in `g`
 	"""
+	# TODO need account for the case when length(g.clusters) < n_clusters
 	return sort(collect(g.clusters), by=length, rev=true)[1:n_clusters]
 end
 
 
-function update_clusters(g::Network, cluster₁::Set{Int}, cluster₂::Set{Int})
+function update_clusters(g::Network, t::Int, cluster₁::Set{Int}, cluster₂::Set{Int})
 	"""
 	Updates `g` with the newly merged cluster the largest cluster size
 	INPUT
 		`g`       : An instance of type Network
+		`t`       : Current step in the evolution process
 		`cluster₁`: Any cluster within `g`
 		`cluster₂`: Any cluster within `g`
 	OUTPUT
@@ -41,17 +43,18 @@ function update_clusters(g::Network, cluster₁::Set{Int}, cluster₂::Set{Int})
 	"""
 	filter!(cluster -> cluster ∉ (cluster₁, cluster₂), g.clusters)
 	merged_cluster = cluster₁ ∪ cluster₂
-	C = maximum((g.C[end], length(merged_cluster)))
 	push!(g.clusters, merged_cluster)
-	push!(g.C, C)
+	C = maximum((g.C[t], length(merged_cluster)))
+	g.C[t+1] = C
 end
 
 
-function update_clusters(g::Network, cluster₁::Set{Int}, cluster₂::Set{Int}, merged_cluster::Set{Int})
+function update_clusters(g::Network, t::Int, cluster₁::Set{Int}, cluster₂::Set{Int}, merged_cluster::Set{Int})
 	"""
 	Updates `g` with the newly merged cluster and the largest cluster size
 	INPUT
 		`g`             : An instance of type Network
+		`t`             : Current step in the evolution process
 		`cluster₁`      : Any cluster within `g`
 		`cluster₂`      : Any cluster within `g`
 		`merged_cluster`: cluster₁ ∪ cluster₂
@@ -59,7 +62,7 @@ function update_clusters(g::Network, cluster₁::Set{Int}, cluster₂::Set{Int},
 		None, updates `g` in-place
 	"""
 	filter!(cluster -> cluster ∉ (cluster₁, cluster₂), g.clusters)
-	C = maximum((g.C[end], length(merged_cluster)))
 	push!(g.clusters, merged_cluster)
-	push!(g.C, C)
+	C = maximum((g.C[t], length(merged_cluster)))
+	g.C[t+1] = C
 end
--- code/Percolation.jl/src/evolution_processes.jl
+++ code/Percolation.jl/src/evolution_processes.jl
@@ -1,35 +1,38 @@
-function erdos_renyi(g::Network, τ::Int)
+function erdos_renyi(g::Network)
 	"""
 	Erdos-Renyi style graph evolution, adds edges randomly at each step
 	INPUT
 		`g`: An instance of type Network
-		`τ`: Total number of edges to add to `g`
 	OUTPUT
 		`g`: Evolved verion of input `g`
 	"""
-	for t in 1:τ
-		add_edge(g, choose_edge(g))
+	for t in 1:g.τ
+		edge = choose_edge(g)
+		add_edge(g, edge)
+		cluster₁ = get_cluster(g, edge[1])
+		cluster₂ = get_cluster(g, edge[2])
+		update_clusters(g, t, cluster₁, cluster₂)
 	end
 	g.P = g.C ./ g.n
 	return g
 end
 
 
-function bohman_frieze(g::Network, τ::Int)
-	for t in 1:τ
+function bohman_frieze(g::Network)
+	for t in 1:g.τ
+	end
 end
 
 
-function product_rule(g::Network, τ::Int)
+function product_rule(g::Network)
 	"""
 	Achlioptas process, implementation of da Costa's product rule
 	INPUT
 		`g`: An instance of type Network
-		`τ`: Total number of edges to add to `g`
 	OUTPUT
 		`g`: Evolved verion of input `g`
 	"""
-	for t in 1:τ
+	for t in 1:g.τ
 		edge₁ = choose_edge(g)
 		edge₂ = choose_edge(g)
 		cluster₁ = get_cluster(g, edge₁[1])
@@ -38,10 +41,10 @@ function product_rule(g::Network, τ::Int)
 		cluster₄ = get_cluster(g, edge₂[2])
 		if length(cluster₁) * length(cluster₂) < length(cluster₃) * length(cluster₄)
 			add_edge(g, edge₁)
-			update_clusters(g, cluster₁, cluster₂)
+			update_clusters(g, t, cluster₁, cluster₂)
 		else
 			add_edge(g, edge₂)
-			update_clusters(g, cluster₃, cluster₄)
+			update_clusters(g, t, cluster₃, cluster₄)
 		end
 	end
 	g.P = g.C ./ g.n
@@ -49,25 +52,25 @@ function product_rule(g::Network, τ::Int)
 end
 
 
-function new_rule(g::Network, τ::Int, q::Float64)
+function new_rule(g::Network, q::Float64)
 	"""
 	Achlioptas process, stochastic process for adding edges to `g`
 	INPUT
 		`g`: An instance of type Network
-		`τ`: Total number of edges to add to `g`
+		`q`: Minimum probability that the chosen edge is accepted
 	OUTPUT
 		`g`: Evolved verion of input `g`
 	"""
-	for t in 1:τ
+	for t in 1:g.τ
 		edge = choose_edge(g)
 		cluster₁ = get_cluster(edge[1])
 		cluster₂ = get_cluster(edge[2])
 		merged_cluster = cluster₁ ∪ cluster₂
-		r = length(merged_cluster) / g.C[end]
+		r = length(merged_cluster) / g.C[t]
 		p = maximum(1-r, q)
 		if rand(g.rng) < p
 			add_edge(g, edge)
-			update_clusters(g, cluster₁, cluster₂, merged_cluster)
+			update_clusters(g, t, cluster₁, cluster₂, merged_cluster)
 		end
 	end
 	g.P = g.C ./ g.n
