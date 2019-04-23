0ac7aca67358e928303f969fc44193e620bead35
Cameron Perot <cameronperot@protonmail.ch>
2019-04-23 12:42:06 +0200

    Added tau parameter to Network type, initialized C array

--- code/Percolation.jl/src/graphs.jl
+++ code/Percolation.jl/src/graphs.jl
@@ -5,11 +5,14 @@ mutable struct Network <: Graph
 	"""
 	Type to house the sets of nodes, edges, and clusters of a network
 	INPUT
-		`n`: Total number of nodes in the network
+		`n`   : Total number of nodes in the network
+		`τ`   : (optional kwarg) Number of edges to add to the graph
+		`seed`: (optional kwarg) Seed value for the random number generator
 	OUTPUT
 		`g`: A new instance of type Network
 	VARIABLES
 		`n`       : Number of nodes
+		`τ`       : Number of edges to add to the graph
 		`nodes`   : Set of nodes present in the graph
 		`edges`   : Set of edges present in the graph
 		`clusters`: Set of clusters present in the graph
@@ -18,19 +21,21 @@ mutable struct Network <: Graph
 		`rng`     : Random number generator
 	"""
 	n        ::Int
+	τ        ::Int
 	nodes    ::Set{Int}
 	edges    ::Set{Tuple{Int, Int}}
 	clusters ::Set{Set{Int}}
 	C        ::Array{Int, 1}
 	P        ::Array{Float64, 1}
 	rng      ::MersenneTwister
-	function Network(n::Int; seed=8)
+	function Network(n::Int; τ=0, seed=8)
 		nodes    = Set(1:n)
 		edges    = Set()
 		clusters = Set()
-		C        = [1]
+		C        = zeros(Int, τ+1)
+		C[1]     = 1
 		P        = []
 		rng      = MersenneTwister(seed)
-		new(n, nodes, edges, clusters, C, P, rng)
+		new(n, τ, nodes, edges, clusters, C, P, rng)
 	end
 end
