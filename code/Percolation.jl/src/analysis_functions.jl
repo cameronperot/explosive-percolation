0a3875b3946d107cba961fd1a806d32cfe40f636
Cameron Perot <cameronperot@protonmail.ch>
2019-05-02 22:14:43 +0200

    Added function to compute the Delta t in Achlioptas et al.

---
+++ code/Percolation.jl/src/analysis_functions.jl
@@ -0,0 +1,15 @@
+function compute_Δ_achlioptas(g::AbstractGraph)
+	"""
+	Determines Δ, t₀, and t₁ for the given evolved `g`
+	Arguments
+		`g`   : An evolved instance of type AbstractGraph
+	Return
+		Three-tuple (Δ, t₀, t₁)
+		`Δ` : t₁ - t₀
+		`t₀`: The last step in the evolution process satisfying `g.C < sqrt(g.n)`
+		`t₁`: The first step in the evolution process satisfying `g.C > 0.5g.n`
+	"""
+	t₀ = sum(g.C .< sqrt(g.n))
+	t₁ = sum(g.C .<= 0.5g.n) + 1
+	return (t₁-t₀, t₀, t₁)
+end
